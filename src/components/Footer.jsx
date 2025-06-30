ba27d13b1df392bc0791894d13f925b6ca122ad3
Aditya <Adityapandey18501@gmail.com>
2025-06-30 15:51:17 +0530

    updated image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="py-8 px-6 border-t border-gray-200">
         <div className="max-w-4xl mx-auto text-center text-gray-600">
-          <p>&copy; 2025 Your Name. All rights reserved.</p>
+          <p>&copy; 2025 Aditya Pandey. All rights reserved.</p>
         </div>
       </footer>
   )
